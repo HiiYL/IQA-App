3569e269a8f776165c9a88d2d193f02f760464bf
HiiYL <yonglian146@gmail.com>
2017-06-05 14:16:28 +0800

    Added voice recognition

---
+++ IQABot/Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import IQABotTests
+
+XCTMain([
+    testCase(IQABotTests.allTests),
+])
--- IQABot/ViewController.swift
+++ IQABot/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import Alamofire
+import Speech
 
 extension UIImage {
     func resized(withPercentage percentage: CGFloat) -> UIImage? {
@@ -26,19 +27,107 @@ extension UIImage {
         return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,SFSpeechRecognizerDelegate {
     
     let captureSession = AVCaptureSession()
     let stillImageOutput = AVCaptureStillImageOutput()
     var previewLayer : AVCaptureVideoPreviewLayer?
     
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
     
+    private let audioEngine = AVAudioEngine()
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    
+    
+    @IBOutlet weak var microphoneButton: UIButton!
     @IBOutlet weak var viewfinderView: UIView!
     // If we find a device we'll store it here for later use
     var captureDevice : AVCaptureDevice?
 
     @IBOutlet weak var askTextField: UITextField!
     @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func microphoneTapped(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.askTextField.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+                self.saveToCamera()
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        self.askTextField.text = "Say something, I'm listening!"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         askTextField.delegate = self
@@ -95,7 +184,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    
     func saveToCamera() {
         if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
             stillImageOutput.captureStillImageAsynchronously(from: videoConnection) {
@@ -118,7 +206,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
                                 multipartFormData.append(value.data(using: String.Encoding.utf8)!, withName: key)
                             }
                     },
-                        to: "https://65a68797.ngrok.io",
+                        //to: "https://65a68797.ngrok.io",
+                        to: "http://140.109.135.211:5000",
                         encodingCompletion: { encodingResult in
                             switch encodingResult {
                             case .success(let upload, _, _):
@@ -137,6 +226,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
                                 }
                             case .failure(let encodingError):
                                 print(encodingError)
+                                self.answerLabel.text = "error"
                             }
                     }
                     )
